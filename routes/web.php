2301c23e2be1849bc87ca71752557e3138232e74
xjEthan <1157763194@qq.com>
2020-03-16 01:47:03 +0000

    注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+// 静态页面
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+// /signup 与 signup 从使用上并无区别
+Route::get('signup', 'UserController@create')->name('signup');
