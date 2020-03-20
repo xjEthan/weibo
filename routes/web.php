af3b54468c91e0b517746dedd0229df4586c7010
xjEthan <1157763194@qq.com>
2020-03-20 06:10:38 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // /signup 与 signup 从使用上并无区别
-Route::get('signup', 'UserController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
