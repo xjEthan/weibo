38b8ed0095c88c2d904db08133440602cdeb11f2
xjEthan <1157763194@qq.com>
2020-03-22 08:32:01 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 // /signup 与 signup 从使用上并无区别
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@detory')->name('logout');
+
